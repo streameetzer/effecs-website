e1ad07ef64bb62dc379293c0ef5a49cca2d19e76
valooford <valooford@outlook.com>
2025-07-01 19:40:56 +0300

    Update GitHub pages deployment config

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -22,8 +22,9 @@ const config: Config = {
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'streameetzer', // Usually your GitHub org/user name.
+  projectName: 'effecs-website', // Usually your repo name.
+  trailingSlash: false,
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
